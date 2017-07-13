e9eed506d3767eb07b58eecb03389a45e3a19ce6
Matthew Hwang <hw4ng_m@yahoo.com>
2017-07-12 21:48:01 -0700

    add index pug

--- Server.js
+++ Server.js
@@ -2,30 +2,34 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
+const pug = require('pug');
 
-router.use(function (req,res,next) {
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+router.use(function (req, res, next) {
   console.log("/" + req.method);
   next();
 });
 
-router.get("/",function(req,res){
-  res.sendFile(path + "index.html");
+router.get("/", function(req, res){
+  res.render('index');
 });
 
-router.get("/about",function(req,res){
-  res.sendFile(path + "about.html");
+router.get("/about", function(req, res){
+  res.render('about');
 });
 
-router.get("/contact",function(req,res){
-  res.sendFile(path + "contact.html");
+router.get("/contact", function(req, res){
+  res.render('contact');
 });
 
 app.use("/",router);
 
-app.use("*",function(req,res){
+app.use("*", function(req, res){
   res.sendFile(path + "404.html");
 });
 
-app.listen(3000,function(){
+app.listen(3000, function(){
   console.log("Live at Port 3000");
 });
