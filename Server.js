452fca47c1c477d3ad132317f6136e7c53160a40
Matthew Hwang <hw4ng_m@yahoo.com>
2017-07-18 17:48:31 -0700

    first major update

--- Server.js
+++ Server.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var app = express();
+var path = require('path');
 var router = express.Router();
 var path = __dirname + '/views/';
 const pug = require('pug');
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+app.use(express.static('public'));
 
 router.use(function (req, res, next) {
   console.log("/" + req.method);
@@ -16,19 +19,15 @@ router.get("/", function(req, res){
   res.render('index');
 });
 
-router.get("/about", function(req, res){
+router.get("/About", function(req, res){
   res.render('about');
 });
 
-router.get("/contact", function(req, res){
-  res.render('contact');
+router.get("/Projects", function(req, res){
+  res.render('projects');
 });
 
 app.use("/",router);
-
-app.use("*", function(req, res){
-  res.sendFile(path + "404.html");
-});
 
 app.listen(3000, function(){
   console.log("Live at Port 3000");
